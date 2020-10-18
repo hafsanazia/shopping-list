eb791ea1d5faebad847ff62c9664806dc3b9de05
hafsanazia <hafsanazia94@gmail.com>
2020-10-18 19:14:44 +0530

    changes in routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,12 @@ import { MatIconModule, MatInputModule, MatCardModule } from '@angular/material'
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CartComponent } from './cart/cart.component';
-import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchPipe } from './search.pipe';
 import { SharedService } from './service/shared.service';
 import { MatButtonModule } from '@angular/material/button';
 
-const appRoutes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
-  { path: "cart", component: CartComponent }
-];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +22,6 @@ const appRoutes: Routes = [
     SearchPipe,
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
